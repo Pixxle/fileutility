879e938053eb1063524a88b82f90267ae6edf437
Dennis Hedlund <dennis.hedlund@klarna.com>
2018-11-30 16:18:17 +0100

    Changed from standalone program to package, whoops

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='fileutility',  
-     version='0.4',
-     scripts=['fileutility'] ,
+     version='0.6',
      author="Dennis Hedlund",
      author_email="dennishedlund@gmail.com",
      description="Helper module for locating and deleting files and folders.",
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/Pixxle/fileutility",
-     packages=setuptools.find_packages(),
+     packages=['fileutility'],
+     zip_safe=False,
      classifiers=[
          "Programming Language :: Python :: 2",
          "License :: OSI Approved :: MIT License",
